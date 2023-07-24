fade88bfce23a6df06c0348d232238f639d2db2a
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-07-23 20:24:48 -0400

    Icons dynamics

---
+++ src/components/getIcon.jsx
@@ -0,0 +1,23 @@
+import * as Bs from "react-icons/bs";
+import * as Ai from "react-icons/ai";
+import * as Hi from "react-icons/hi";
+import * as Tb from "react-icons/tb";
+
+const GetIcon = ({ icon, className }) => {
+  const getIcon = (iconName) => {
+    const iconsMap = new Map();
+    iconsMap.set("Bs", Bs);
+    iconsMap.set("Ai", Ai);
+    iconsMap.set("Hi", Hi);
+    iconsMap.set("Tb", Tb);
+
+    return iconsMap.get(iconName.substring(0, 2));
+  };
+
+  const icons = getIcon(icon);
+  const TheIcon = icons[icon];
+
+  return <TheIcon className={className} />;
+};
+
+export default GetIcon;
